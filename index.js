0a5b495347d12451747bbcba3cd26f4f5f18ee46
Nihal <nihallaskar888@gmail.com>
2025-12-08 23:44:56 +0600

    added 2 get apis for loan

---
+++ index.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require("cors")
+app.use(cors())
+
+const uri = process.env.URI;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+async function run() {
+    try {
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        const db = client.db('loansDB')
+        const loansCollection = db.collection('loans')
+
+      
+        app.get('/Allloans', async (req, res) => {
+
+            const result = await loansCollection.find().toArray()
+            res.send(result)
+        })  
+        //  home loans
+        app.get('/Homeloans', async (req, res) => {
+
+            const result = await loansCollection.find().limit(6).toArray()
+            res.send(result)
+        })
+        // detail api
+        app.get('/loans/:id', async (req, res) => {
+            const id = req.params.id
+            const result = await loansCollection.findOne({ _id: new ObjectId(id) })
+            res.send(result)
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
